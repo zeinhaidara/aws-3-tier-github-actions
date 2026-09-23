f1229d7e5a966b442da23dcb3cded2ba5774963b
Zein <moulaye.haidara@techconsulting.tech>
2026-09-23 13:29:08 -0400

    Refresh ASG when launch template changes

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -263,7 +263,7 @@ resource "aws_autoscaling_group" "app" {
 
   launch_template {
     id      = aws_launch_template.app.id
-    version = "$Latest"
+    version = aws_launch_template.app.latest_version
   }
 
   tag {
